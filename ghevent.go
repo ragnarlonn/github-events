68c9eeb18a69538dd17f97fe8972c3c78908b9d2
Ragnar Lonn <ragnar@lonn.org>
2020-09-24 14:28:25 +0200

    ...forgot to add this one with the new TestStruct type

--- ghevent.go
+++ ghevent.go
@@ -2,14 +2,150 @@ package ghevent
 
 import "time"
 
+type PushEvent struct {
+	Ref        *string     `json:"ref"`
+	Before     *string     `json:"before"`
+	After      *string     `json:"after"`
+	Created    *bool       `json:"created"`
+	Deleted    *bool       `json:"deleted"`
+	Forced     *bool       `json:"forced"`
+	BaseRef    *string     `json:"base_ref"`
+	Compare    *string     `json:"compare"`
+	Commits    []Commit    `json:"commits"`
+	HeadCommit *Commit     `json:"head_commit"`
+	Repository *Repository `json:"repository"`
+	Pusher     *EmailUser  `json:"pusher"`
+	Sender     *User       `json:"sender"`
+}
+
+type ForkEvent struct {
+	Forkee     *Repository `json:"forkee"`
+	Repository *Repository `json:"repository"`
+	Sender     *User       `json:"sender"`
+}
+
+type IssueCommentEvent struct {
+	Action     *string       `json:"action"`
+	Issue      *Issue        `json:"issue"`
+	Comment    *IssueComment `json:"comment"`
+	Repository *Repository   `json:"repository"`
+	Sender     *User         `json:"sender"`
+}
+
+type IssuesEvent struct {
+	Action *string `json:"action"`
+	Issue  *Issue  `json:"issue"`
+	// Changes ??? object `json:"changes"`
+	Repository *Repository `json:"repository"`
+	Sender     *User       `json:"sender"`
+}
+
+type LabelEvent struct {
+	Action     *string     `json:"action"`
+	Label      *Label      `json:"label"`
+	Repository *Repository `json:"repository"`
+	Sender     *User       `json:"sender"`
+}
+
+// type MarketplacePurchaseEvent struct {
+
+type PullRequestEvent struct {
+	Action      *string      `json:"action"`
+	Number      *int         `json:"number"`
+	PullRequest *PullRequest `json:"pull_request"`
+	Repository  *Repository  `json:"repository"`
+	Sender      *User        `json:"sender"`
+}
+
+//
+// Objects sent in the various events
+//
+
+type IssueComment struct {
+	URL               *string    `json:"url"`
+	HTMLURL           *string    `json:"html_url"`
+	IssueURL          *string    `json:"issue_url"`
+	ID                *int       `json:"id"`
+	NodeID            *string    `json:"node_id"`
+	User              *User      `json:"user"`
+	CreatedAt         *time.Time `json:"created_at"`
+	UpdatedAt         *time.Time `json:"updated_at"`
+	AuthorAssociation *string    `json:"author_association"`
+	Body              *string    `json:"body"`
+}
+
+type Issue struct {
+	ID               *int              `json:"id"`
+	NodeID           *string           `json:"node_id"`
+	URL              *string           `json:"url"`
+	RepositoryURL    *string           `json:"repository_url"`
+	LabelsURL        *string           `json:"labels_url"`
+	CommentsURL      *string           `json:"comments_url"`
+	EventsURL        *string           `json:"events_url"`
+	HTMLURL          *string           `json:"html_url"`
+	Number           *int              `json:"number"`
+	State            *string           `json:"state"`
+	Title            *string           `json:"title"`
+	Body             *string           `json:"body"`
+	User             *User             `json:"user"`
+	Labels           []Label           `json:"labels"`
+	Assignee         *User             `json:"assignee"`
+	Assignees        []User            `json:"assignees"`
+	Milestone        *Milestone        `json:"milestone"`
+	Locked           *bool             `json:"locked"`
+	ActiveLockReason *string           `json:"active_lock_reason"`
+	Comments         *int              `json:"comments"`
+	PullRequest      *IssuePullRequest `json:"pull_request"`
+	ClosedAt         *time.Time        `json:"closed_at"`
+	CreatedAt        *time.Time        `json:"created_at"`
+	UpdatedAt        *time.Time        `json:"updated_at"`
+	ClosedBy         *User             `json:"closed_by"`
+}
+
+type IssuePullRequest struct {
+	URL      *string `json:"url"`
+	HTMLURL  *string `json:"html_url"`
+	DiffURL  *string `json:"diff_url"`
+	PatchURL *string `json:"patch_url"`
+}
+
+type Label struct {
+	ID          *int    `json:"id"`
+	NodeID      *string `json:"node_id"`
+	URL         *string `json:"url"`
+	Name        *string `json:"name"`
+	Description *string `json:"description"`
+	Color       *string `json:"color"`
+	Default     *bool   `json:"default"`
+}
+
+type Milestone struct {
+	URL          *string    `json:"url"`
+	HTMLURL      *string    `json:"html_url"`
+	LabelsURL    *string    `json:"labels_url"`
+	ID           *int       `json:"id"`
+	NodeID       *string    `json:"node_id"`
+	Number       *int       `json:"number"`
+	State        *string    `json:"state"`
+	Title        *string    `json:"title"`
+	Description  *string    `json:"description"`
+	Creator      *User      `json:"creator"`
+	OpenIssues   *int       `json:"open_issues"`
+	ClosedIssues *int       `json:"closed_issues"`
+	CreatedAt    *time.Time `json:"created_at"`
+	UpdatedAt    *time.Time `json:"updated_at"`
+	ClosedAt     *time.Time `json:"closed_at"`
+	DueOn        *time.Time `json:"due_on"`
+}
+
 type Repository struct {
-	ID               int       `json:"id"`
-	NodeID           string    `json:"node_id"`
-	Name             string    `json:"name"`
-	FullName         string    `json:"full_name"`
-	Private          bool      `json:"private"`
-	Owner            User      `json:"owner"`
-	HTMLURL          string    `json:"html_url"`
+	ID               *int      `json:"id"`
+	NodeID           *string   `json:"node_id"`
+	Name             *string   `json:"name"`
+	FullName         *string   `json:"full_name"`
+	Private          *bool     `json:"private"`
+	Owner            *User     `json:"owner"`
+	HTMLURL          *string   `json:"html_url"`
 	Description      string    `json:"description"`
 	Fork             bool      `json:"fork"`
 	URL              string    `json:"url"`
@@ -115,22 +251,6 @@ type User struct {
 	UpdatedAt         time.Time `json:"updated_at"`
 }
 
-type PushEvent struct {
-	Ref        string     `json:"ref"`
-	Before     string     `json:"before"`
-	After      string     `json:"after"`
-	Created    bool       `json:"created"`
-	Deleted    bool       `json:"deleted"`
-	Forced     bool       `json:"forced"`
-	BaseRef    string     `json:"base_ref"`
-	Compare    string     `json:"compare"`
-	Commits    []Commit   `json:"commits"`
-	HeadCommit Commit     `json:"head_commit"`
-	Repository Repository `json:"repository"`
-	Pusher     EmailUser  `json:"pusher"`
-	Sender     User       `json:"sender"`
-}
-
 type CommitUser struct {
 	Email string    `json:"email"`
 	Name  string    `json:"name"`
@@ -174,6 +294,10 @@ type Commit struct {
 	Author      User         `json:"author"`
 	Committer   User         `json:"committer"`
 	Parents     []TreeObject `json:"parents"`
+}
+
+type PullRequest struct {
+	// XXX TODO
 }
 
 /*
@@ -314,3 +438,18 @@ type Commit struct {
 }
 
 */
+
+//
+// TestStruct is used to test that JSON encoding/decoding works as intended
+//
+
+type TestStruct struct {
+	IntegerField     *int      `json:"int"`
+	FloatField       *float64  `json:"float"`
+	StringField      *string   `json:"string"`
+	BoolField        *bool     `json:"bool"`
+	StringSliceField []string  `json:"stringslice"`
+	IntSliceField    []int     `json:"intslice"`
+	FloatSliceField  []float64 `json:"floatslice"`
+	BoolSliceField   []bool    `json:"boolslice"`
+}
